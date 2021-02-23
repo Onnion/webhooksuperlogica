9f3a8544750bb455dac72ba617d2f3d229a46d8c
Marcio Henrique <marcio.henrique@ramper.com.br>
2021-02-23 14:40:47 -0300

    init [2]

--- app.js
+++ app.js
@@ -9,16 +9,20 @@ const router = express.Router();
 
 app.use(jsonParser);
 app.use(cors());
+app.use(express.static('logs'))
 
 const log = (json) => {
     const date = new Date();
-    fs.appendFileSync(path.join('./', 'log.txt'), `${date}:\n${JSON.stringify(json)}\n\n`);
+    const format = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+    fs.appendFileSync(path.join('./logs', `${format}.txt`), `${format}: \n${JSON.stringify(json)}\n\n`);
 }
 
 router.post('/log', (req, res) => {
     log(req.body);
     res.status(200).send({ "status": 200 });
 });
+
+
 
 app.use(router);
 app.listen(process.env.PORT || 3000);
